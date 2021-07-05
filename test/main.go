6ecb24ca9a76f1cb0646dc23e8e91871ef71c712
AmarKumar-AK <9801amarkumar@gmail.com>
2021-07-05 15:09:25 +0530

    created and reversed linklist without methods

--- test/main.go
+++ test/main.go
@@ -1,64 +1,47 @@
 package main
-import "fmt"
 
-type node struct {
-	data int
-	next *node
-}
-
-type linkedlist struct {
-	length int
-	head  *node
-}
-
-func (l *linkedlist) Push(val int) {
-	new_node := node{val,nil}
-	if l.head == nil {
-		l.head = &new_node
-		l.length = l.length + 1
-	} else {
-		new_node.next = l.head
-		l.head = &new_node
-		l.length = l.length + 1
-	}
-}
-
-func (l linkedlist) Printing() {
-	if l.head == nil {
-		return
-	}
-	curr := l.head
-	for curr != nil {
-		fmt.Print(curr.data," ")
-		curr = curr.next
-	}
-	fmt.Println("done printing")
-}
-
-func (l *linkedlist) reverse(){
-	//prev := &node
-	var prev *node = nil
-	var nxt *node = nil
-	curr := l.head
-	for curr!= nil {
-		nxt = curr.next
-		curr.next = prev
-		prev = curr
-		curr = nxt
-	}
-	l.head = prev
-}
 
 func main() {
-	li := &linkedlist{}
-	li.Push(10)
-	li.Push(20)
-	li.Push(30)
-	li.Push(40)
-	li.Push(50)
-	li.Printing()
-	li.reverse()
-	li.Printing()
+	/////////////// code 1 ///////////////////////
+	Push(10)
+	Push(20)
+	Push(30)
+	Push(40)
+	Push(50)
+	Print()
+	Reverse()
+	Print()
+	/////////////// code 2 /////////////////////
+	//li := &LinkedList{}
+	//li.Push(10)
+	//li.Push(20)
+	//li.Push(30)
+	//li.Push(40)
+	//li.Push(50)
+	//li.Printing()
+	//li.Reverse()
+	//li.Printing()
+
 
 }
 /////////////////////////////////////////////////////////////////////////////
+//package main
+//
+//import "fmt"
+//
+//var vis [1001]int
+//
+//
+//func dfs(src int) {
+//	if vis[src]==1 {
+//		return
+//	}
+//	vis[src] = 1
+//	fmt.Println(src)
+//	for data :
+//}
+//func main() {
+//	for i:=1 ; i<=10 ; i++ {
+//		vis[i] = 0
+//	}
+//}
