dda24876550c1db049e760223b03d197be3859b9
AmarKumar-AK <9801amarkumar@gmail.com>
2021-07-06 11:25:01 +0530

    using multiple parameters in function

--- test/main.go
+++ test/main.go
@@ -2,20 +2,46 @@ package main
 
 import "fmt"
 
-func main() {
-	r := rectangle{5,6}
-	c := circle{7}
-	s := square{4}
-	fmt.Println("area : ",r.area(), c.area(), s.area())
-	fmt.Println("perimeter : ",r.perimeter(),c.perimeter(),s.perimeter())
-
-	shapes := []shape{r,c,s}
-
-	fmt.Println("Hello Amar")
-	for _, data := range shapes {
-		//fmt.Println(data.area(), data.perimeter())
-		fmt.Println(findArea(data))
+func add(num ...int) int{
+	sum:=0
+	for _,data :=range  num {
+		sum += data
 	}
+	return sum
+}
+func main() {
+
+	sum:=add(1,2,3)
+	fmt.Println("1st ",sum)
+	a := []int{1,2,3,4,5}
+	sum = add(a...)
+	fmt.Println("2nd ",sum)
+
+	////////////////////////////////////
+	//if n:= rand.Int31n(20) ; n%2 ==0 {
+	//	fmt.Println("even")
+	//	fmt.Println("value of n inside if: ",n)
+	//} else {
+	//	fmt.Println("odd")
+	//	fmt.Println("value of n inside else : ",n)
+	//	n:=n + 1
+	//	fmt.Println(n)
+	//}
+	//fmt.Println("value of n : ",n)
+	////////////////////////////////////////////////////
+	//r := rectangle{5,6}
+	//c := circle{7}
+	//s := square{4}
+	//fmt.Println("area : ",r.area(), c.area(), s.area())
+	//fmt.Println("perimeter : ",r.perimeter(),c.perimeter(),s.perimeter())
+	//
+	//shapes := []shape{r,c,s}
+	//
+	//fmt.Println("Hello Amar")
+	//for _, data := range shapes {
+	//	//fmt.Println(data.area(), data.perimeter())
+	//	fmt.Println(findArea(data))
+	//}
 	/////////////// code 1 ///////////////////////
 	//Push(10)
 	//Push(20)
